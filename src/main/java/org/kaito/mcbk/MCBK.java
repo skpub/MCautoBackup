8a48ca7150f284cb715ea90212118d60863dbd8c
Sato Kaito <satodeyannsu@gmail.com>
2023-06-05 12:01:08 +0900

    first

---
+++ src/main/java/org/kaito/mcbk/MCBK.java
@@ -0,0 +1,17 @@
+package org.kaito.mcbk;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCBK extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
